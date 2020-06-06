7d8f9f0f13bfde4a77deaa24fb628c9dec00188b
hieubkvn123 <hieubkvn123@gmail.com>
2020-06-06 23:24:24 +0800

    ahihi

--- img_processing/object_recognition_hog/test.py
+++ img_processing/object_recognition_hog/test.py
@@ -5,14 +5,17 @@ import pandas as pd
 '''
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
-'''
 from sklearn.model_selection import train_test_split
+'''
 
 ### NONONONO ###
 
 ### self-written machine learning, kinda slow but good enuff ###
 from svm import KernelSVM 
 from pca import pca
+
+# now the moment of truth, comparing to sklearn
+from sklearn.svm import SVC 
 
 def accuracy_score(predictions, labels):
 	accurate_count = 0
@@ -39,6 +42,25 @@ def standardize(x):
 			x_std = x/v_length
 
 			return x_std
+
+def train_test_split(x, y, test_size=0.3):
+	n_test = int(x.shape[0] * test_size)
+	n_train = x.shape[0] - n_test 
+
+	idx = np.array(list(range(x.shape[0])))
+	np.random.shuffle(idx)
+
+	idx_train = idx[:n_train]
+	idx_test = idx[n_train:x.shape[0]]
+
+	x_train = x[idx_train]
+	y_train = y[idx_train]
+
+	x_test = x[idx_test]
+	y_test = y[idx_test]
+
+	# print(idx_train, idx_test)
+	return x_train, x_test, y_train, y_test
 
 # ss = StandardScaler()
 
@@ -67,10 +89,16 @@ y = raw_data['outcome'].to_numpy()
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
 model = KernelSVM()
-model.fit(x_train,y_train, alpha = 0.01, iterations=200)
-
+model.fit(x_train,y_train, alpha = 0.01, iterations=100)
 
 predictions = model.predict(x_test)
 accuracy = accuracy_score(predictions, y_test)
 
-print(accuracy)
+print("[INFO] Home made recipe : " + str(accuracy))
+
+model = SVC(kernel='rbf')
+model.fit(x_train, y_train)
+
+predictions = model.predict(x_test)
+accuracy = accuracy_score(predictions, y_test)
+print("[INFO] Sklearn's recipe : " + str(accuracy))
