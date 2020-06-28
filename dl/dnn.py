0dc3ea14c0fd9b7fc09af15dfffc7c4b7ac80897
hieubkvn123 <hieubkvn123@gmail.com>
2020-06-28 23:22:54 +0800

    finished Naive Bayes

--- dl/dnn.py
+++ dl/dnn.py
@@ -11,22 +11,4 @@ class NeuralNet(object):
 		self.output = None 
 		self.theta = None 
 
-	def optimizer(self, algo='sgd', beta_1=0.9,beta_2=0.999):
-		operation = lambda g, w, alpha : w - alpha * g
-
-		def adam(g, w, i, m_t, g_t, alpha):
-			m_t = beta_1 * m_t + (1-beta_1)*g 
-			v_t = beta_2 * v_t + (1-beta_2)*(g**2)
-
-			m_t_hat = m_t/(1-beta_1**i)
-			v_t_hat = v_t/(1-beta_2**i)
-
-			w = m_t_hat/(math.sqrt(v_t_hat) + 1e-08)
-
-			return w, m_t, v_t 
-                def sgd(g, w, alpha):
-
-		if(algo == 'adam'):
-			operation = lambda g, t  
-
-	def fit(self, x, y, lr=1e-3, optimizer='sgd'):
+	def optimizer(self):
--- naive_bayes.py
+++ naive_bayes.py
@@ -6,4 +6,103 @@ matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
 # this is naive bayes implementation
+class_1 = np.array([[1, 2, 3], [1, 1, 2], [2, 2, 2],
+                    [2, 1, 2], [2, 2, 1], [1, 3, 1],
+                    [3, 2, 1], [3, 1, 3], [3, 3, 3]])
 
+class_2 = np.array([[4, 2, 4], [4, 5, 3], [4, 4, 4],
+                    [5, 6, 7], [5, 5, 5], [4, 5, 4],
+                    [5, 6, 5], [6, 5, 6], [6, 6, 6]])
+
+x = np.concatenate((class_1, class_2))
+y = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1])
+
+x_test = np.array([[1,2,2], [2,1,1], [3,2,2], [4,2,4], [6,3,2], [4,5,3]])
+y_test = np.array([1, 1, 1, -1, -1, -1])
+
+class NaiveBayes(object):
+    def __init__(self):
+        self.x = np.array([])
+        self.y = np.array([])
+
+    def get_probability_y(self, y_i):
+        ### Get the probability of each class happening ###
+        y_i = self.y[np.where(self.y == y_i)]
+        N_y_i = y_i.shape[0]
+
+        P_yi = float( N_y_i / self.N )
+
+        return P_yi
+
+    def get_probability_x_given_y(self, x, y_i):
+        ### the probability of x happining given y ###
+        
+        # first, get the training set where y = y_i
+        x_train = self.x[np.where(self.y == y_i)]
+        train_size = x_train.shape[0]
+
+        occurences = list()
+
+        ### Loop through the testing data ###
+        for i in range(x.shape[0]):
+            occurence_count = 0
+            for j in range(x_train.shape[0]):
+                if(x[i] in x_train[j]):
+                    occurence_count += 1
+
+            occurences.append(occurence_count)
+
+        ### calculate probability of all x_i ###
+        P_xi_given_y = 1
+        for occurence in occurences:
+            P_xi_given_y *= occurence
+
+        return P_xi_given_y
+
+    def fit(self, x, y):
+        if(x.shape[0] == y.shape[0]):
+            self.x = x
+            self.y = y
+            self.N = x.shape[0]
+        else:
+            print("[INFO] Input and output must have the same length ...")
+            
+    def predict(self, x):
+        if(len(x.shape) < 2):
+            print("[INFO] Input data must be an array of vectors ... ")
+            return None
+
+        if(x.shape[1] != self.x.shape[1]):
+            print("[INFO] Input must have the same shape as the training dataset")
+            return None
+
+        outputs = list()
+
+        for i in range(x.shape[0]):
+            probabilities = list()
+            for y_i in self.y:
+                p = self.get_probability_y(y_i) * self.get_probability_x_given_y(x[i], y_i)
+                probabilities.append(p)
+
+            class_ = self.y[np.argmax(probabilities)]
+            outputs.append(class_)
+
+        return outputs
+
+
+def accuracy_score(y_test, y):
+    correct = 0
+    for i, y_ in enumerate(y_test):
+        if(y_ == y[i]):
+            correct += 1
+
+    accuracy = float(correct / y.shape[0])
+
+    return "{0:.2f}".format(accuracy)
+
+nb = NaiveBayes()
+nb.fit(x, y)
+output = nb.predict(x_test)
+
+accuracy = accuracy_score(output, y_test)
+print("[INFO] Test accuracy : " + str(accuracy))
