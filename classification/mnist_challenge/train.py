4d295f713e7b3a188e1bc8ac94d1dad5dbe89aa1
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-10-08 09:08:03 -0400

    First commit

--- classification/mnist_challenge/train.py
+++ classification/mnist_challenge/train.py
@@ -5,15 +5,21 @@ from data_loader import train_images, train_labels
 from data_loader import test_images, test_labels
 from data_loader import train_labels_original, test_labels_original
 
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-
 BATCH_SIZE=60
+
+train_images = train_images[:6000]
+train_labels = train_labels[:6000]
+
+test_images = test_images[:1000]
+test_labels = test_labels[:1000]
+
+train_images = train_images / 255.0
+test_images  = test_images /  255.0
 
 class Discriminator():
     def __init__(self):
         self.weights = np.random.rand(train_images.shape[1], train_labels.shape[1])
-        self.penalty = 0.3
+        self.penalty = 0.0002
         self.batch   = 32
         self.eps = 1e-8
         self.lr = 1e-3
@@ -41,7 +47,7 @@ class Discriminator():
         predictions = self.forward(features, targets)
         
         loss = self.softmax_crossentropy(targets, predictions)
-        print(loss.mean())
+        loss = loss.mean()
 
         gradients = - (X.transpose() @ (targets - predictions))
         self.momentum = self.beta_1 * self.momentum + (1 - self.beta_1) * gradients
@@ -52,7 +58,8 @@ class Discriminator():
 
         # self.weights = self.weights - self.lr * gradients
         self.weights = self.weights - self.lr * momentum_/(np.sqrt(velocity_) + self.eps)
-
+        
+        return loss
 
     def forward(self, x, y):
         x = x / np.expand_dims(np.linalg.norm(x, axis=1), axis=1)
@@ -69,23 +76,48 @@ class Discriminator():
 
         return out
 
-    def train(self, trainX, trainY, epochs=200):
+    def transform(self, x):
+        x = x / np.expand_dims(np.linalg.norm(x, axis=1), axis=1)
+        W = self.weights / np.expand_dims(np.linalg.norm(self.weights, axis=0), axis=0)
+
+        logits = x @ W
+        
+        return logits 
+
+    def train(self, trainX, trainY, epochs=20):
         num_batches = trainX.shape[0] // self.batch + 1
+        previous_loss = np.inf
         for i in range(epochs):
+            losses = []
             for j in range(num_batches):
                 X = trainX[j*self.batch:(j+1)*self.batch]
                 Y = trainY[j*self.batch:(j+1)*self.batch]
-                self.train_step(i, Y, X)
+                loss = self.train_step(i, Y, X)
+                losses.append(loss)
+
+            final_loss = np.array(losses)
+            final_loss = final_loss[~np.isnan(final_loss)]
+            final_loss = final_loss.mean()
+
+            if(final_loss > previous_loss):
+                print('[*] Early stopping ... ')
+                break
+            previous_loss = final_loss
+
+            print('Epoch {}, loss = {}'.format(i+1, final_loss))
 
 disc = Discriminator()
-disc.train(train_images, train_labels)
+disc.train(train_images, train_labels, epochs=1000 )
 
-X = train_images @ disc.weights
+### Generate embeddings ###
+print('[*] Generate embeddings ... ')
+X = disc.transform(train_images)
+X_ = disc.transform(test_images)
 
-X_ = test_images @ disc.weights
+X /= np.linalg.norm(X, axis=1, keepdims=True)
+X_ /= np.linalg.norm(X_, axis=1, keepdims=True)
+Y_ = np.argmax(X_, axis=1)
 
-svm = SVC(kernel='rbf', C=1.0, gamma='auto', probability=True)
-svm.fit(X, train_labels_original)
-
-predictions = svm.predict(X_)
-accuracy = accuracy_score(predictions, test_labels_original)
+print('[*] making reference from testing set')
+accuracy = accuracy_score(Y_, test_labels_original[:1000])
+print(accuracy)
